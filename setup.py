358c4ec59a18c1ead76f67a743ef8ecf7a0a28ae
Antonin Blanc <antonin.blanc@ipsosante.fr>
2018-12-05 00:06:50 +0000

    Version 0.1

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='logforwarder-python',
+    version='0.1',
+    description='Python logforwarder logging handler',
+    author='ipso sante',
+    author_email='contact@ipsosante.fr',
+    url='https://github.com/ipsosante/logforwarder-python',
+    long_description='',
+    packages=find_packages(),
+    zip_safe=False,
+)
